f4355a6c3b440df63d0abe3074d9047a6cef98d6
Daniel Alvarado <teslafloyd@gmail.com>
2022-03-29 18:54:27 -0500

    Create test

---
+++ AdministradorProductos/test/co/endava/app/AdministradorProductosTest.java
@@ -0,0 +1,12 @@
+package co.endava.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdministradorProductosTest {
+
+    @Test
+    void main() {
+    }
+}
